70e3e97e6c701528cd296a04eb775103b4896499
Djonathan <djonathanmachadoo@gmail.com>
2024-03-08 12:51:23 -0300

    att

--- calculoIdade/index.js
+++ calculoIdade/index.js
@@ -1,15 +1,15 @@
-let idade = parseInt(prompt("Digite a sua idade"))
-let mes = parseInt(prompt("Digite o mês em que você nasceu"))
-let ano1 = parseInt(prompt("Digite o ano em que você nasceu"))
+let idade = parseInt(prompt("Digite a sua idade"));
+let mes = parseInt(prompt("Digite o mês em que você nasceu"));
+let ano1 = parseInt(prompt("Digite o ano em que você nasceu"));
 
-dias = 365,25
-mes = 30
-ano2 = 2024
+let dias = parseFloat(365,25);
+let mes2 = parseFloat(30);
+let ano2 = parseInt(2024);
 
-let resultado = (idade * dias)
-let resultado2 = (resultado / mes)
-let resultado3 = (ano2 - ano1)
+let resultado = (idade * dias);
+let resultado2 = (resultado / mes2);
+let resultado3 = (ano2 - ano1);
 
-console.log("Seus dias de vida: " + resultado)
-console.log("Seus meses: " + resultado2)
-console.log("Seus anos: " + resultado3)
+console.log("Seus dias de vida: " + resultado);
+console.log("Seus meses: " + resultado2);
+console.log("Seus anos: " + resultado3);
